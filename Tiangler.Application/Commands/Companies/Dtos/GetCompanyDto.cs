4c68a5fdb5f6451fd907055835491f17a9c66f3c
Alex <alexc@boostandco.com>
2022-11-26 18:08:05 +0800

    create endpoint to get user companies

---
+++ Tiangler.Application/Commands/Companies/Dtos/GetCompanyDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tiangler.Core.Domains.Companies;
+
+namespace Tiangler.Application.Commands.Companies.Dtos
+{
+    public class GetCompanyDto
+    {
+        public GetCompanyDto(Company company)
+        {
+            CompanyId = company.CompanyId;
+            CompanyName = company.CompanyName;
+
+        }
+
+        public Guid CompanyId { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
---
+++ Tiangler.Application/Commands/Companies/Interfaces/IGetUserCompaniesCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tiangler.Application.Commands.Companies.Dtos;
+using Tiangler.Core.ResultModels;
+
+namespace Tiangler.Application.Commands.Companies.Interfaces
+{
+    public interface IGetUserCompaniesCommand
+    {
+        Task<CommandResult<List<GetCompanyDto>>> ExecuteAsync(Guid userId);
+    }
+}
